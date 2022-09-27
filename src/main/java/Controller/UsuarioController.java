9f11bd55b3f244845f7cebd5c4ceee5742315e8a
paulo maia <pauloafmaia9@gmail.com>
2022-09-27 11:44:16 -0300

    Criação usuario e usuariocontroller

---
+++ src/main/java/Controller/UsuarioController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UsuarioController {
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("Login/login");
+        return mv;
+    }
+}
