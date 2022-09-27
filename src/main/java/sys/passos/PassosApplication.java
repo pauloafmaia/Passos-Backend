94494577d00609b51971b53d6441cfb73dad3173
paulo maia <pauloafmaia9@gmail.com>
2022-09-27 11:16:34 -0300

    First Commit

---
+++ src/main/java/sys/passos/PassosApplication.java
@@ -0,0 +1,13 @@
+package sys.passos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sys/passos/PassosApplicationTests.java
@@ -0,0 +1,13 @@
+package sys.passos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
