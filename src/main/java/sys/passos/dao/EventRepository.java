502bfea7ba1ffe961ed5906dde95d60c38457e91
Paulo Maia <100380603+pauloafmaia@users.noreply.github.com>
2024-02-26 17:00:43 -0300

    Delete src/main/java/sys/passos/dao/EventRepository.java

--- src/main/java/sys/passos/dao/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package sys.passos.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sys.passos.model.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
