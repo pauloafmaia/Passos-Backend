4447947da22d6dc1bff810f44ea5cf750909042a
Paulo Maia <100380603+pauloafmaia@users.noreply.github.com>
2024-02-26 17:00:51 -0300

    Delete src/main/java/sys/passos/dao/SongRepository.java

--- src/main/java/sys/passos/dao/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package sys.passos.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sys.passos.model.Song;
-
-public interface SongRepository extends JpaRepository <Song, Long> {
-}
