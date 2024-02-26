632765b5b991d56d65363df118dd1a0d38aef423
Paulo Maia <100380603+pauloafmaia@users.noreply.github.com>
2024-02-26 16:59:43 -0300

    Update UserController.java

--- src/main/java/sys/passos/controller/UserController.java
+++ src/main/java/sys/passos/controller/UserController.java
@@ -5,51 +5,62 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sys.passos.dao.UserRepository;
 import sys.passos.model.User;
+import sys.passos.service.UserService;
+
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserService userService;
 
-    @GetMapping("/user")
+
+    @GetMapping
     public List<User> getUser() {
         return userRepository.findAll();
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity findById(@PathVariable long id) {
         return userRepository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
+                .map(record -> ResponseEntity.status(200).body(record))
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/user")
-    public User create (@RequestBody User user){
-        return userRepository.save(user);
+    @PostMapping
+    public ResponseEntity create (@RequestBody User user){
+        userRepository.save(user);
+        if (user != null) {
+            return ResponseEntity.status(200).body("User created!");
+        } else {
+            return ResponseEntity.status(400).body("User was not created");
+        }
     }
 
-    @PutMapping("/user/{id}")
-    public ResponseEntity update (@PathVariable("id") long id, @RequestBody User user){
+    @PutMapping("/{id}")
+    public ResponseEntity update (@PathVariable long id, @RequestBody User user){
         return userRepository.findById(id)
                 .map(record -> {
                     record.setLogin(user.getLogin());
                     record.setPassword(user.getPassword());
+                    record.setEmail(user.getEmail());
                     User updated = userRepository.save(record);
-                    return ResponseEntity.ok().body(updated);
-                  }).orElse(ResponseEntity.notFound().build());
+                    return ResponseEntity.ok().body("User was updated successfully!");
+                  }).orElse(ResponseEntity.status(404).build());
     }
 
-    @DeleteMapping("/user/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity delete (@PathVariable long id){
         return userRepository.findById(id)
                 .map(record -> {
                     userRepository.deleteById(id);
-                    return ResponseEntity.ok().build();
-                }).orElse(ResponseEntity.notFound().build());
+                    return ResponseEntity.status(200).body("User was removed!");
+                }).orElse(ResponseEntity.status(404).build());
     }
 
 }
