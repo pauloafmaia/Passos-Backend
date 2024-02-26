55a3692b07780c5c1bc2ece3a162c76d57263480
Paulo Maia <100380603+pauloafmaia@users.noreply.github.com>
2024-02-26 17:01:44 -0300

    Delete src/main/java/sys/passos/exception/EventNotFoundException.java

--- src/main/java/sys/passos/exception/EventNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package sys.passos.exception;
-
-public class EventNotFoundException extends RuntimeException{
-
-    public EventNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
