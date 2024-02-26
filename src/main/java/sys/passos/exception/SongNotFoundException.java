a8daab3c8c454e41be08de77672d401a7908b5d1
Paulo Maia <100380603+pauloafmaia@users.noreply.github.com>
2024-02-26 17:01:51 -0300

    Delete src/main/java/sys/passos/exception/SongNotFoundException.java

--- src/main/java/sys/passos/exception/SongNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package sys.passos.exception;
-
-public class SongNotFoundException extends RuntimeException{
-
-    public SongNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
